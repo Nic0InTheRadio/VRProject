0918f9945705973ccd0ed3233c7b688b8a124cc9
KaitoOwU <advestankv@gmail.com>
2024-12-03 13:42:04 +0100

    Add FCTween

---
+++ Source/VRProject/Public/Door.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Door.generated.h"
+
+UCLASS()
+class VRPROJECT_API ADoor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ADoor();
+
+	UFUNCTION(BlueprintCallable)
+	void OpenDoor();
+
+};
