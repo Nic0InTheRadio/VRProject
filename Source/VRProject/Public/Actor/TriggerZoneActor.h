7c1214a9fb8570ca550857d70101cc137d24341a
Dan <dan.cheype@gmail.com>
2024-12-06 10:54:14 +0100

    [EDIT] Removed useless CPP

--- Source/VRProject/Public/Actor/TriggerZoneActor.h
+++ Source/VRProject/Public/Actor/TriggerZoneActor.h
@@ -14,28 +14,19 @@ class VRPROJECT_API ATriggerZoneActor : public AActor
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this actor's properties
 	ATriggerZoneActor();
 
 	virtual void Tick(float DeltaTime) override;
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-	
-	// Collision component for the trigger zone
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Trigger Zone")
-	UBoxComponent* TriggerZone;
 
-	// Optional: Sound to play
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Trigger Zone")
 	USoundBase* TriggerSound;
 
-	// Optional: Animation to play
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Trigger Zone")
 	UAnimSequence* TriggerAnimation;
 
-	// Trigger logic
 	UFUNCTION()
 	void OnTriggerEnter(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 						UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
